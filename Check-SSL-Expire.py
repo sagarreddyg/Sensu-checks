e559f63070a20bb943ba1e46f068bab9e7179623
sagarreddyg <gantasagarreddy467@gmail.com>
2019-11-09 17:22:52 +0530

    SSL check added

---
+++ Check-SSL-Expire.py
@@ -0,0 +1,104 @@
+# -*- encoding: utf-8 -*-
+# requires a recent enough python with idna support in socket
+# pyopenssl, cryptography and idna
+
+from OpenSSL import SSL
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+import idna
+import datetime
+from socket import socket
+from collections import namedtuple
+from datetime import date
+
+HostInfo = namedtuple(field_names='cert hostname peername', typename='HostInfo')
+#('www.bestprice.in', 443)
+HOSTS = [
+    ('www.capillarytech.com', 443),
+    ('www.pizzahut.co.za', 443),
+    ('mon-dashboard.capillarytech.cn.com', 443),
+    ('www.gait.com.kw',443),
+    ('www.bestprice.in',443)
+]
+
+def verify_cert(cert, hostname):
+    # verify notAfter/notBefore, CA trusted, servername/sni/hostname
+    cert.has_expired()
+    # service_identity.pyopenssl.verify_hostname(client_ssl, hostname)
+    # issuer
+
+def get_certificate(hostname, port):
+    hostname_idna = idna.encode(hostname)
+    sock = socket()
+
+    sock.connect((hostname, port))
+    peername = sock.getpeername()
+    ctx = SSL.Context(SSL.SSLv23_METHOD) # most compatible
+    ctx.check_hostname = False
+    ctx.verify_mode = SSL.VERIFY_NONE
+
+    sock_ssl = SSL.Connection(ctx, sock)
+    sock_ssl.set_connect_state()
+    sock_ssl.set_tlsext_host_name(hostname_idna)
+    sock_ssl.do_handshake()
+    cert = sock_ssl.get_peer_certificate()
+    crypto_cert = cert.to_cryptography()
+    sock_ssl.close()
+    sock.close()
+
+    return HostInfo(cert=crypto_cert, peername=peername, hostname=hostname)
+
+def get_alt_names(cert):
+    try:
+        ext = cert.extensions.get_extension_for_class(x509.SubjectAlternativeName)
+        return ext.value.get_values_for_type(x509.DNSName)
+    except x509.ExtensionNotFound:
+        return None
+
+def get_common_name(cert):
+    try:
+        names = cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)
+        return names[0].value
+    except x509.ExtensionNotFound:
+        return None
+
+def get_issuer(cert):
+    try:
+        names = cert.issuer.get_attributes_for_oid(NameOID.COMMON_NAME)
+        return names[0].value
+    except x509.ExtensionNotFound:
+        return None
+
+
+def critical(hostinfo):
+    daysleft = 0
+    daysover = 0
+    da = datetime.date.today()
+    d1 = date(hostinfo.cert.not_valid_after.year, hostinfo.cert.not_valid_after.month,
+              hostinfo.cert.not_valid_after.day)
+    d2 = date(da.year, da.month, da.day)
+    daysi = d1 - d2
+    exdays = daysi
+    if exdays.days < 0:
+        print("CRITICAL : {} SSL certificate is expired on {} days: {} ".format(hostinfo.hostname,
+                                                                                    hostinfo.cert.not_valid_after,
+                                                                                    daysi.days))
+    elif exdays.days <= 10:
+        print("CRITICAL : {} SSL certificate will expire on {} days: {} ".format(hostinfo.hostname,
+                                                                                    hostinfo.cert.not_valid_after,
+                                                                                    daysi.days))
+    elif exdays.days <=30:
+        print("WARNING : {} SSL certificate will expire on {} days: {} ".format(hostinfo.hostname,
+                                                                                     hostinfo.cert.not_valid_after,
+                                                                                     daysi.days))
+
+
+def check_it_out(hostname, port):
+    hostinfo = get_certificate(hostname, port)
+
+
+import concurrent.futures
+if __name__ == '__main__':
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as e:
+        for hostinfo in e.map(lambda x: get_certificate(x[0], x[1]), HOSTS):
+                critical(hostinfo)
